6d8f64b3c8a2ab2f4aa618db7bd9c6b9db30aa82
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-17 18:20:38 +0530

    feat: price vise filtering

--- script.js
+++ script.js
@@ -169,3 +169,35 @@ document.querySelector(".title-filter").addEventListener("click", async () => {
       });
     });
 });
+
+document.querySelector(".price-filter").addEventListener("click", async () => {
+  container.style.display = "none";
+  container2.style.display = "none";
+  container3.style.display = "none";
+  container4.style.display = "flex";
+  container4.style.flexWrap = "wrap";
+  navbar.after(container4);
+  let val = document.querySelector(".title-filter").textContent;
+  fetch(baseURL + "products?sortBy=price&order=asc")
+    .then((data) => data.json())
+    .then((data) => {
+      data.products.map((product) => {
+        let productCard = document.createElement("div");
+        let pImage = document.createElement("img");
+        let pTitle = document.createElement("p");
+        let pPrice = document.createElement("span");
+
+        productCard.classList.add("product-card");
+        productCard.setAttribute("data-val", product.id);
+
+        pImage.setAttribute("src", product.thumbnail);
+        pTitle.textContent = product.title;
+        pPrice.textContent = product.price;
+
+        container4.append(productCard);
+        productCard.append(pImage);
+        productCard.append(pTitle);
+        productCard.append(pPrice);
+      });
+    });
+});
