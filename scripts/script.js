21f079588610eac249f8628b0f4e098c1ffc9bbd
Nishiv Trivedi <Nishiv@techeniac.com>
2026-08-25 14:57:49 +0530

    feat: products can be updated now

--- scripts/script.js
+++ scripts/script.js
@@ -175,6 +175,7 @@ function productPageFunc() {
           let p_rating = document.createElement("p");
           let p_warranty = document.createElement("p");
           let p_deleteButton = document.createElement("button");
+          let p_editButton = document.createElement("button");
 
           container.style.display = "none";
           container2.style.display = "none";
@@ -192,6 +193,7 @@ function productPageFunc() {
           dataContainer.style.gap = "20px";
           dataContainer.style.marginTop = "10%";
           p_deleteButton.style.width = "100px";
+          p_editButton.style.width = "100px";
 
           p_image.setAttribute("src", data.thumbnail);
           p_title.textContent = data.title;
@@ -201,6 +203,7 @@ function productPageFunc() {
           p_rating.textContent = "Rating: " + data.rating;
           p_warranty.textContent = "Warranty: " + data.warrantyInformation;
           p_deleteButton.textContent = "Delete";
+          p_editButton.textContent = "Edit";
 
           productPage.replaceChildren();
           section.appendChild(productPage);
@@ -214,6 +217,7 @@ function productPageFunc() {
           dataContainer.append(p_rating);
           dataContainer.append(p_warranty);
           dataContainer.append(p_deleteButton);
+          dataContainer.append(p_editButton);
 
           p_deleteButton.addEventListener("click", () => {
             fetch("https://dummyjson.com/products/" + id, {
@@ -224,6 +228,98 @@ function productPageFunc() {
                 alert("Item Deleted | isDeleted: " + res.isDeleted);
                 console.log(res);
               });
+          });
+
+          p_editButton.addEventListener("click", () => {
+            container.style.display = "none";
+            container2.style.display = "none";
+            container3.style.display = "none";
+            productPage.style.display = "none";
+            productForm.style.display = "block";
+            pagination.classList.add("hide");
+            const form = document.createElement("form");
+            const title = document.createElement("input");
+            const desc = document.createElement("input");
+            const image = document.createElement("input");
+            const price = document.createElement("input");
+            const stock = document.createElement("input");
+            const submit = document.createElement("input");
+
+            productForm.replaceChildren();
+            title.setAttribute("type", "text");
+            title.setAttribute("name", "title");
+            title.setAttribute("placeholder", "Update Product Name");
+            desc.setAttribute("type", "text");
+            desc.setAttribute("name", "description");
+            desc.setAttribute("placeholder", "Update Product Description");
+            image.setAttribute("type", "file");
+            image.setAttribute("name", "thumbnail");
+            price.setAttribute("type", "number");
+            price.setAttribute("name", "price");
+            price.setAttribute("placeholder", "Update Product Price");
+            stock.setAttribute("type", "number");
+            stock.setAttribute("name", "stock");
+            stock.setAttribute("placeholder", "Update Product Stock");
+            submit.setAttribute("type", "submit");
+
+            form.classList.add("product-form");
+            title.classList.add("input-ele");
+            desc.classList.add("input-ele");
+            price.classList.add("input-ele");
+            stock.classList.add("input-ele");
+            submit.classList.add("input-ele");
+
+            section.appendChild(productForm);
+            productForm.appendChild(form);
+            form.appendChild(title);
+            form.appendChild(desc);
+            form.appendChild(image);
+            form.appendChild(price);
+            form.appendChild(stock);
+            form.appendChild(submit);
+
+            form.onsubmit = (e) => {
+              e.preventDefault();
+              const obj = {
+                title: title.value,
+                description: desc.value,
+                thumbnail: image,
+                price: price.value,
+                stock: stock.value,
+              };
+
+              let formData = new FormData();
+              title.value && formData.append("title", title.value);
+              desc.value && formData.append("description", desc.value);
+              image.value && formData.append("thumbnail", image);
+              price.value && formData.append("price", price.value);
+              stock.value && formData.append("stock", stock.value);
+
+              fetch("https://dummyjson.com/products/" + id, {
+                method: "PUT",
+                //   headers: { "Content-Type": "application/json" },
+                // body: new FormData(formData),
+                body: formData,
+                // body: JSON.stringify(obj),
+              })
+                .then((res) => {
+                  if (!res.ok) {
+                    alert("Something went wrong");
+                    return;
+                  }
+                  return res.json();
+                })
+                .then((data) => {
+                  alert(
+                    "Product Updated | id: " +
+                      data.id +
+                      " | Updated Name: " +
+                      data.title,
+                  );
+                  console.log(data);
+                })
+                .catch((err) => console.log(err));
+            };
           });
         })
         .catch((err) => console.log(err));
